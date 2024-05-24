28d838432995ff4bf58d3a8a9eb9e3ee0ff12717
Eva <krageleva67@gmail.com>
2024-05-24 10:11:03 +0300

    delete viewModel

--- app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -10,18 +10,5 @@ import com.example.shoppinglist.domain.usecase.EditShopItemUseCase
 import com.example.shoppinglist.domain.usecase.GetShopListUseCase
 
 class MainViewModel : ViewModel() {
-    private val repositoryImpl = ImplShopListRepository///change
-    private val getShopListUseCase = GetShopListUseCase(repositoryImpl)
-    private val editShopItemUseCase = EditShopItemUseCase(repositoryImpl)
-    private val deleteShopItemUseCase = DeleteShopItemUseCase(repositoryImpl)
-    val shopList = getShopListUseCase.getShopList()
 
-    fun deleteItem(shopItem: ShopItem) {
-        deleteShopItemUseCase.deleteItem(shopItem)
-    }
-
-    fun changeEnabledState(shopItem: ShopItem) {
-        val newItem = shopItem.copy(enabled = !shopItem.enabled)
-        editShopItemUseCase.editItem(newItem)
-    }
 }
