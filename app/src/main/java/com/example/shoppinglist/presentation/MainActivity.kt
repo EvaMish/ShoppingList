f4b1818a5df9f2f339302089c13df07224cf3857
Eva <krageleva67@gmail.com>
2024-06-05 17:20:24 +0300

    add new fun

--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -8,22 +8,36 @@ import com.example.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
-    private lateinit var adapter: ShopListAdapter
+    private lateinit var shopListAdapter: ShopListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setUpRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            adapter.shopList=it
+            shopListAdapter.shopList=it
             //Log.d("++++++++", it.toString())
         }
     }
 
     private fun setUpRecyclerView(){
         val rvShopList=findViewById<RecyclerView>(R.id.rv_shop_list)
-        adapter= ShopListAdapter()
-        rvShopList.adapter=adapter
+        with(rvShopList) {
+            shopListAdapter = ShopListAdapter()
+            adapter = shopListAdapter
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.ENABLED_VIEW_TYPE,
+                ShopListAdapter.VIEW_POOL_SIZE
+            )
+            recycledViewPool.setMaxRecycledViews(
+                ShopListAdapter.DISABLED_VIEW_TYPE,
+                ShopListAdapter.VIEW_POOL_SIZE
+            )
+            shopListAdapter.onshopItemLongClickListener = {
+                viewModel.changeEnabledStateItem(it)
+            }
+
+        }
     }
 
 }
