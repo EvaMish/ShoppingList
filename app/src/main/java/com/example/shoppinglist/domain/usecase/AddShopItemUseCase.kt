da6dd8124edd891df55905e4fd0eee749326eb3f
Eva <krageleva67@gmail.com>
2024-06-05 17:11:42 +0300

    add listener

--- app/src/main/java/com/example/shoppinglist/domain/usecase/AddShopItemUseCase.kt
+++ app/src/main/java/com/example/shoppinglist/domain/usecase/AddShopItemUseCase.kt
@@ -3,7 +3,8 @@ package com.example.shoppinglist.domain.usecase
 import com.example.shoppinglist.domain.ShopItem
 import com.example.shoppinglist.domain.ShopListRepository
 
-class AddShopItemUseCase(private val shopListRepository: ShopListRepository) {
+class
+AddShopItemUseCase(private val shopListRepository: ShopListRepository) {
     fun addItem(shopItem: ShopItem) {
         shopListRepository.addItem(shopItem)
     }
